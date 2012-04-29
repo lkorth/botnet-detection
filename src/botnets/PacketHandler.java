997277caa60e826bc8e2533906bc4695dd6a58a8
Luke Korth <luke@lukekorth.com>
2012-04-29 12:52:37 -0500

    Clean up DNS query conversion to string.

--- src/botnets/PacketHandler.java
+++ src/botnets/PacketHandler.java
@@ -19,19 +19,18 @@ class PacketHandler implements PacketReceiver {
 		if(packet.header[23] == 17) { //udp packets (IP Packet protocol 17)
 			UDPPacket p = (UDPPacket) packet;
 			if(p.dst_port == 53) { //dns outbound queries only
-				System.out.println(new String(p.data)); //print data of dns query
-				//System.out.println(p);
+				System.out.println(convert(p.data)); //print data of dns query
 			}
 		}
 	}
 	
 	//Conversation from byte[] to string
-	String convert(byte[] data) {
+	private String convert(byte[] data) {
 	    StringBuilder sb = new StringBuilder(data.length);
-	    for (int i = 0; i < data.length; ++ i) {
+	    for (int i = 13; i < data.length-5; ++ i) {
 	        if (data[i] < 0) throw new IllegalArgumentException();
 	        else if (data[i] < 32) sb.append('.');
-	        else sb.append((char) data[i]);
+	        else if (data[i] > 32 && data[i] < 127) sb.append((char) data[i]);
 	    }
 	    return sb.toString();
 	}
