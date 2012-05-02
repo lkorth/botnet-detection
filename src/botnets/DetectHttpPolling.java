250b8325045d94679adf847a1dbadd11e44ce999
Luke Korth <luke@lukekorth.com>
2012-05-02 01:39:09 -0500

    Added DetectHttpPolling class

---
+++ src/botnets/DetectHttpPolling.java
@@ -0,0 +1,5 @@
+package botnets;
+
+public class DetectHttpPolling { //Adapt perl code developed here: http://security.utexas.edu/consensus/HTTP%20Bot%20Detection.pdf
+	//TODO Implement Polling detection
+}
